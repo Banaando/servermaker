6986210465331d6768755c1505502d5f04d538d1
Banaando <ericdan1130@gmail.com>
2025-02-27 20:36:37 -0500

    Rewrote a bunch of things to be cleaner, but it still kinda sucks.

--- src/Server.java
+++ src/Server.java
@@ -3,63 +3,42 @@ import org.apache.commons.io.IOUtils;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
 public class Server {
-    private String downloadURL;
-    private String version;
-    private String type;
-    private URL versionManifestURL;
-    private JSONObject versionManifestJSON;
-    private String latest;
-    private String latestSnapshot;
-    private JSONArray versions;
-    private JSONObject config;
-    private JSONObject versionJSON;
-    private URL versionURL;
-    private JSONObject versionJSON2;
+    // Config variables retrieved by getServerConfig
+    private boolean agreedToEula;
+    private String serverType;
+    private String serverVersion;
+    private int javaVersion;
+    private String serverMemory;
+    private int serverPort;
+    private String serverMOTD;
+    private String javaFlags;
+    private JSONObject serverConfigFile;
+
+    // Retrieved by getServerJSONURL
+    private String serverJSONURL;
+    private String serverDownloadURL;
 
     public Server() throws IOException {
-        versionManifestURL = new URL("https://piston-meta.mojang.com/mc/game/version_manifest_v2.json");
-        versionManifestJSON = getJson(versionManifestURL);
-        assert versionManifestJSON != null;
-        versions = versionManifestJSON.getJSONArray("versions");
-        String configContent = new String(Files.readAllBytes(Paths.get("/Users/eric/IdeaProjects/servermaker/src/ServerMakerConfig.json")));
-        config = new JSONObject(configContent);
-        type = config.getString("server-type");
-        version = config.getString("server-version");
-        latest = versionManifestJSON.getJSONObject("latest").getString("release");
-        latestSnapshot = versionManifestJSON.getJSONObject("latest").getString("snapshot");
-
-        if(version.equals("latest")) {
-            version = latest;
-        } else if(version.equals("latest-snapshot")) {
-            version = latestSnapshot;
-        }
-
-        for(int i = 0; i < versions.length(); i++) {
-            versionJSON = versions.getJSONObject(i);
-            if(versionJSON.getString("id").equals(version)) {
-                versionURL = new URL(versionJSON.getString("url"));
-                break;
-            }
-        }
-
-        versionJSON2 = getJson(versionURL);
-        assert versionJSON2 != null;
-        downloadURL = versionJSON2.getJSONObject("downloads").getJSONObject("server").getString("url");
-
+        getServerConfig();
+        serverJSONURL = getServerJSONURL(serverType);
+        serverDownloadURL = getServerDownloadURL(serverJSONURL);
     }
 
     public void downloadJar() throws IOException {
-        FileUtils.copyURLToFile(new URL(downloadURL), new File(version + ".jar"));
+        System.out.println("Downloading server jar...");
+        FileUtils.copyURLToFile(new URL(serverDownloadURL), new File(serverVersion + ".jar"));
     }
 
-    public static JSONObject getJson(URL url) {
+    public static JSONObject getJSONFromURL(URL url) {
         try {
             String json = IOUtils.toString(url, StandardCharsets.UTF_8);
             return new JSONObject(json);
@@ -67,5 +46,81 @@ public class Server {
             e.printStackTrace();
             return null;
         }
+    }
+
+    public void generateStartScript() {
+        try {
+            FileWriter linuxStartScript = new FileWriter("start.sh");
+            linuxStartScript.write("you still have laundry on our bed.");
+            linuxStartScript.close();
+
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    private void getServerConfig() throws IOException {
+        String serverConfigContent = new String(Files.readAllBytes(Paths.get("/Users/eric/IdeaProjects/servermaker/src/ServerMakerConfig.json")));
+        serverConfigFile = new JSONObject(serverConfigContent);
+        agreedToEula = serverConfigFile.getBoolean("agreed-to-eula");
+        serverType = serverConfigFile.getString("server-type");
+        serverVersion = getServerVersion(serverType);
+        javaVersion = serverConfigFile.getInt("java-version");
+        serverMemory = serverConfigFile.getString("memory");
+        serverPort = serverConfigFile.getInt("port");
+        serverMOTD = serverConfigFile.getString("motd");
+        javaFlags = serverConfigFile.getString("java-flags");
+    }
+
+    private String getServerVersion(String serverType) throws MalformedURLException {
+        if(serverConfigFile.getString("server-version").equals("latest")) {
+            switch(serverType) {
+                case "vanilla": {
+                    URL versionManifestURL = new URL("https://piston-meta.mojang.com/mc/game/version_manifest_v2.json");
+                    JSONObject versionManifestJSON = getJSONFromURL(versionManifestURL);
+                    return versionManifestJSON.getJSONObject("latest").getString("release");
+                }
+                case "paper": {
+
+                }
+            }
+        } else if(serverConfigFile.getString("server-version").equals("latest-snapshot")) {
+            switch(serverConfigFile.getString("server-type")) {
+                case "vanilla": {
+                    URL versionManifestURL = new URL("https://piston-meta.mojang.com/mc/game/version_manifest_v2.json");
+                    JSONObject versionManifestJSON = getJSONFromURL(versionManifestURL);
+                    return versionManifestJSON.getJSONObject("latest").getString("snapshot");
+                }
+                case "paper": {
+
+                }
+            }
+        }
+        return serverConfigFile.getString("server-version");
+    }
+
+    private String getServerJSONURL(String serverType) throws MalformedURLException {
+        switch(serverType) {
+            case "vanilla": {
+                URL versionManifestURL = new URL("https://piston-meta.mojang.com/mc/game/version_manifest_v2.json");
+                JSONObject versionManifestJSON = getJSONFromURL(versionManifestURL);
+                JSONArray versionManifestArray = versionManifestJSON.getJSONArray("versions");
+                for(int i = 0; i < versionManifestArray.length(); i++) {
+                    if(versionManifestArray.getJSONObject(i).getString("id").equals(serverVersion)) {
+                        return versionManifestArray.getJSONObject(i).getString("url");
+                    }
+                }
+            }
+            case "paper": {
+
+            }
+        }
+        return "Server JSON not found";
+    }
+
+    private String getServerDownloadURL(String serverJSONURL) throws MalformedURLException {
+        JSONObject serverJSON = getJSONFromURL(new URL(serverJSONURL));
+        return serverJSON.getJSONObject("downloads").getJSONObject("server").getString("url");
+
     }
 }
--- src/ServerMaker.java
+++ src/ServerMaker.java
@@ -4,5 +4,6 @@ public class ServerMaker {
     public static void main(String[] args) throws IOException {
         Server server = new Server();
         server.downloadJar();
+        server.generateStartScript();
     }
 }
